2b01ec44f98bbac7dcc799fa490001a12a258ee5
mskwarek <marcin.skwarek@mcx.pl>
2017-03-25 11:35:15 +0100

    * models with tests
    Change-Id: I351dc0a61ac24cfa6d9b89f769f4153760137143

--- hello/models.py
+++ hello/models.py
@@ -8,9 +8,9 @@ class Calendar(models.Model):
 class Meeting(models.Model):
     calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
     name = models.CharField(max_length=100)
-    palce = models.DateField(max_length=1024)
-    start_date = models.IntegerField()
+    place = models.CharField(max_length=1024)
+    start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     participant_surname = models.CharField(max_length=512)
     creation_date = models.DateTimeField()
-    color = models.CharField(null=True)
+    color = models.CharField(max_length=128, null=True)
--- hello/tests.py
+++ hello/tests.py
@@ -1,10 +1,13 @@
 from django.test import TestCase
-#from myapp.models import Animal
+from hello.models import Calendar, Meeting
+import datetime
 
 
 class CalendarTestCase(TestCase):
     def setUp(self):
-        return
+        self.cal = Calendar.objects.create(name="TEST")
 
     def test_animals_can_speak(self):
-        return
+        meeting = Meeting(calendar = self.cal, name="test meeting", place="test place", start_date=datetime.datetime.now(),
+                                         end_date=datetime.datetime.now(), participant_surname="abacki")
+
